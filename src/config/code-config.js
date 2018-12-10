a9e3d010156163e92041ebb6b989b450997b6fff
平凡公子 <282729573@qq.com>
2018-12-10 21:16:56 +0800

    feat: 添加常用code状态码及对应信息

---
+++ src/config/code-config.js
@@ -0,0 +1,12 @@
+const error = {
+    '404': {
+        message: '404 not found',
+        header: 'text/plain',
+    },
+    '500': {
+        message: 'server error',
+        header: 'text/plain',
+    },
+};
+
+export default error;
